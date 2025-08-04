60c63e5e50aa7709b1c3c59d710e991b16771ce0
Sunithaupanal <sunitaupanal2003@gmail.com>
2025-08-04 13:36:39 +0530

    Add files via upload

--- src/data/products.js
+++ src/data/products.js
@@ -1,1 +1,52 @@
+const products = [
+    {
+        id: 1,
+        product_name: 'Men Solid Orange',
+        product_price: 499.00,
+        product_image: 'https://ultimez.com/tasks/assets/images/image1.jpeg'
+    },
+    {
+        id: 2,
+        product_name: 'Men Graphic Print',
+        product_price: 451.00,
+        product_image: 'https://ultimez.com/tasks/assets/images/image2.jpeg'
+    },
+    {
+        id: 3,
+        product_name: 'Men Graphic Print',
+        product_price: 599.00,
+        product_image: 'https://ultimez.com/tasks/assets/images/image3.jpeg'
+    },
+    {
+        id: 4,
+        product_name: 'Men Striped Polo',
+        product_price: 479.00,
+        product_image: 'https://ultimez.com/tasks/assets/images/image4.jpeg'
+    },
+    {
+        id: 5,
+        product_name: 'Men Striped Black',
+        product_price: 349.00,
+        product_image: 'https://ultimez.com/tasks/assets/images/image5.jpeg'
+    },
+    {
+        id: 6,
+        product_name: 'Men Typography',
+        product_price: 600.00,
+        product_image: 'https://ultimez.com/tasks/assets/images/image6.jpeg'
+    },
+    {
+        id: 7,
+        product_name: 'Men Printed Hooded',
+        product_price: 334.00,
+        product_image: 'https://ultimez.com/tasks/assets/images/image7.jpeg'
+    },
+    {
+        id: 8,
+        product_name: 'Embroidered Red Shirt',
+        product_price: 453.00,
+        product_image: 'https://ultimez.com/tasks/assets/images/image8.jpeg'
+    }
+];
 
+export default products;
